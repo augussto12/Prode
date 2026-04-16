4f3f47a0a26f97ab70aeebb5c26d8d6195db6ac7
Augusto Alvarez <augustoalvarez10@gmail.com>
2026-04-16 02:29:52 -0300

    fix

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
+import 'dotenv/config'; // Esto asegura que .env cargue ANTES de que se resuelvan los módulos abajo
 import { createServer } from 'http';
 import app from './src/app.js';
-import dotenv from 'dotenv';
 import { initSocket } from './src/socket.js';
-
-dotenv.config();
 
 const server = createServer(app);
 const PORT = process.env.PORT || 5000;
