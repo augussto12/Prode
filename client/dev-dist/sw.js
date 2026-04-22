f57a63ed733d1601c13aea0f9d01b6087bef4f1c
Augusto Alvarez <augustoalvarez10@gmail.com>
2026-04-22 17:24:53 -0300

    fix compose

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ipf4sva23bk"
+    "revision": "0.cs2f18irf5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
